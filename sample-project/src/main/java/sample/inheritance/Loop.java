451eb7959c25c70d35d7ef6fffacdb03435beea9
NissyChacko <chackonissy@gmail.com>
2024-01-21 16:07:36 +0530

    first commit 21/01/24

---
+++ sample-project/src/main/java/sample/inheritance/Loop.java
@@ -0,0 +1,17 @@
+package sample.inheritance;
+import java.util.Scanner;
+public class Loop {
+	
+public static void main(String[] args) {
+	System.out.println("enter a number");
+	Scanner sc = new Scanner(System.in);
+	int j= sc.nextInt();
+	
+	for(int i=0;i<j;i++)
+	
+	System.out.println(i);
+	sc.close();
+	
+ }
+
+}
