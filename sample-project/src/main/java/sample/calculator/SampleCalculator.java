5137c6aabb955c18a515ee1878cc4d72fec58e65
NissyChacko <chackonissy@gmail.com>
2024-01-21 16:11:14 +0530

    first commit

---
+++ sample-project/src/main/java/sample/calculator/SampleCalculator.java
@@ -0,0 +1,17 @@
+package sample.calculator;
+
+
+public class SampleCalculator {
+	public void printCalculator() {
+		System.out.println("Calculator");
+	}
+	
+
+	public static void main(String[] args) {
+		SampleCalculator sampleCalculator = new SampleCalculator();
+		sampleCalculator.printCalculator();
+		
+
+	}
+
+}
